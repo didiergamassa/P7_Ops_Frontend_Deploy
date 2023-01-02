7bd4fafecc76b123c25edf35ab86bac6a0fb412e
didiergamassa <didiergamassa@gmail.com>
2023-01-02 21:45:47 +0100

    add modif

--- front3.py
+++ front3.py
@@ -214,7 +214,7 @@ def main() :
      <div style="background-color: tomato; padding:10px; border-radius:10px">
      <h1 style="color: white; text-align:center">KG_Credit_Bank </h1>
      <h2 style="color: white; text-align:center">Dashboard Scoring Credit/Projet_7/V_01</h2>
-     <h3 style="color: black; text-align:center">Credit Risk Project implemented,by Didier GAMASSA /IT_Project_Manager,with the support of OpenClassrooms</h3>
+     <h3 style="color: black; text-align:center">Credit Risk Project implemented,by Didier GAMASSA(IT_Project_Manager&Data_Scientist),with the support of OpenClassrooms</h3>
      <h2 style="color: Blue; text-align:center">                  </h2>
      </div>
      <p style="font-size: 20px; font-weight: bold; text-align:center">This Dashboard is a decision tool allowing to validate or not loans to applicants.</p>
