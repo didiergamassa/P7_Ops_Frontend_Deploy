1e706add93227bb2eab662e4d5f51782c562db7e
didiergamassa <didiergamassa@gmail.com>
2023-01-02 22:45:26 +0100

    add modif

--- front3.py
+++ front3.py
@@ -214,8 +214,8 @@ def main() :
      <div style="background-color: tomato; padding:10px; border-radius:10px">
      <h1 style="color: white; text-align:center">KG_Credit_Bank </h1>
      <h2 style="color: white; text-align:center">Dashboard Scoring Credit/Projet_7/V_01</h2>
-     <h3 style="color: black; text-align:center">Credit Risk Project implemented by Didier GAMASSA</h3>
-     <h4 style="color: black; text-align:center">*****IT_Project_Manager/Data_Scientist*****</h3>
+     <h3 style="color: black; text-align:center">**Credit Risk Project implemented by Didier GAMASSA**</h3>
+     <h4 style="color: black; text-align:center">IT_Project_Manager/Data_Scientist</h3>
      <h5 style="color: black; text-align:center">Dev_Support provided by OpenClassrooms/GitHub/Heroku_Dev_Center</h3>
           <h2 style="color: Blue; text-align:center">                  </h2>
      </div>
